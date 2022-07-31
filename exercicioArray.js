a289889a2787f02222721a89435af8a06cbbf086
Rfsantos99 <107157045+Rfsantos99@users.noreply.github.com>
2022-07-31 10:44:01 -0300

    Add files via upload

---
+++ exercicioArray.js
@@ -0,0 +1,67 @@
+const data = [
+    {
+      nome: 'Roger Medeiros',
+      sexo: 'M',
+      salario: 3250,
+    },
+    {
+      nome: 'Carolina Silva',
+      sexo: 'F',
+      salario: 1200,
+    },
+    {
+      nome: 'Cristina Avila',
+      sexo: 'F',
+      salario: 8100,
+    },
+    {
+      nome: 'Gustavo Hoffman',
+      sexo: 'M',
+      salario: 5200.35,
+    },
+    {
+      nome: 'Eva Trindade',
+      sexo: 'F',
+      salario: 2501,
+    },
+    {
+      nome: 'Andre Mathias',
+      sexo: 'M',
+      salario: 1750,
+    },
+    {
+      nome: 'Joice Castro da Silva',
+      sexo: 'F',
+      salario: 3350.25,
+    },
+  ];
+
+//1 Imprima no console a quantidade de pessoas Total.
+console.log(`O número total de pessoas é ${data.length}.`)
+
+//2 Imprima no console a quantidade de pessoas pessoas do sexo Feminino.
+console.log(`A quantidade de pessoas do sexo feminino é igual a ${data.filter((fem) => fem.sexo === 'F').length}`)
+
+//3 Imprima no console a soma do salário de todas as pessoas.
+const total = data.reduce((atual, proximo)=> atual+proximo.salario,0)
+console.log(`O valor total da soma dos salários é ${total}.`)
+
+//4 Imprima no console a soma do salario de todos as pessoas do sexo Masculino
+const totalMasc = data.filter((masc) => masc.sexo === 'M').reduce((atual, proximo)=> atual+proximo.salario,0)
+console.log(`A soma do salário do sexo masculino é ${totalMasc}.`)
+
+/*5. Utilize a função Some, para descobrir se existe algum salário
+superior a R$ 7.000, imprima verdadeiro no console caso exista, caso
+contrário falso.*/
+const sheila = data.some(elemento => elemento.salario > 7000)
+console.log(`o salário é maior que 7000? ${sheila}.`)
+
+//6. Utilize a função findIndex, para descobrir a posição da pessoa de nome 'Eva Trindade'.
+const evinha = data.findIndex((valor) => valor.nome === 'Eva Trindade')
+console.log(`Tem alguma Eva Trindade aqui? ${evinha}.`)
+
+//7. Utilize a função filter, para filtrar todas pessoas que o nome possua o sobrenome "Silva".
+const silva = data.filter((sil)=> sil.nome.indexOf('Silva')>=0).map(m=>console.log(m.nome));
+
+//8. Imprima os nomes utilizando o MAP
+const nomes = data.map(item => console.log(item.nome))
